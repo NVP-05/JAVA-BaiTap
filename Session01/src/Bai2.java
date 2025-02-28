50883a9da40f70df9d5d04c28b2e942dd72041bf
NVP-05 <mkPhuc123456789@gmail.com>
2025-02-28 08:11:04 +0700

    add

---
+++ Session01/src/Bai2.java
@@ -0,0 +1,8 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String name = "Nguyễn Văn Phúc";
+        int age = 20;
+        String address = "Thanh Hóa";
+        System.out.println("Tôi lên là " + name + ", tôi " + age + " tuổi, hiện tại đang học tại PTIT-HCM. Quê quán ở " + address);
+    }
+}
---
+++ Session01/src/Bai5.java
@@ -0,0 +1,20 @@
+public class Bai5 {
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------+");
+        System.out.println("|            MENU Chức Năng             |");
+        System.out.println("+---------------------------------------+");
+        System.out.println("| 1.  Đăng nhập                         |");
+        System.out.println("| 2.  Đăng ký                           |");
+        System.out.println("| 3.  Xem danh sách sản phẩm            |");
+        System.out.println("| 4.  Tìm kiếm sản phẩm                 |");
+        System.out.println("| 5.  Thêm sản phẩm vào giỏ hàng        |");
+        System.out.println("| 6.  Xóa sản phẩm khỏi giỏ hàng        |");
+        System.out.println("| 7.  Xem giỏ hàng                      |");
+        System.out.println("| 8.  Thanh toán                        |");
+        System.out.println("| 9.  Xem lịch sử                       |");
+        System.out.println("| 10. Thoát                             |");
+        System.out.println("+---------------------------------------+");
+        System.out.println("| Vui lòng chọn chức năng 1-10          |");
+        System.out.println("+---------------------------------------+");
+    }
+}
